dcbc276cf47037489d47594bab6beea169a06bcf
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-27 16:53:31 -0700

    Add form items into Firebase and show in list.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,25 @@
 
 /* Controllers */
 
-angular.module('checklist.controllers', []).
-  .controller('MyCtrl1', [function() {
+angular.module('checklist.controllers', [])
+  .controller('AddCtrl', [
+    '$scope',
+    'angularFire',
+    function($scope, angularFire) {
+      var url = 'https://mmy-checklist.firebaseio.com/list';
+      var promise = angularFire(url, $scope, 'list', {});
+
+      $scope.myVar = "Start your list here.";
+      $scope.items = [];
+      $scope.addItem = function(){
+        $scope.items.push({text:$scope.text});
+        $scope.list.items = $scope.items;
+        $scope.text = '';
+        console.log(JSON.stringify($scope.items));
+      }
+
+    promise.then(function() {
+      $scope.$watch('list', $scope);
+    })
+
   }]);
