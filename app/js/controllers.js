856155b215ddbfa5e87c01d8c232de4eacf27066
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-28 09:44:34 -0700

    Persist data into database by connecting all array items to Firebase list.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,27 +8,26 @@ angular.module('checklist.controllers', [])
     'angularFire',
     function($scope, angularFire) {
       var url = 'https://mmy-checklist.firebaseio.com/list';
-      var promise = angularFire(url, $scope, 'list', {});
+      var promise = angularFire(url, $scope, 'list', {items: []});
 
-      $scope.items = [];
+      promise.then(function(){
       $scope.addItem = function(){
         if($scope.text){
-          $scope.items.push({text:$scope.text, status:false});
+          $scope.list.items.push({text:$scope.text, status:false});
         }
-        $scope.list.items = $scope.items;
+        $scope.list.items = $scope.list.items;
         $scope.text = '';
-        console.log(JSON.stringify($scope.items));
-        console.log($scope.items.length);
-        console.log($scope.items.length);
+        // console.log(JSON.stringify($scope.items));
+        // console.log($scope.items.length);
       }
 
       $scope.getTotal = function(){
-        return $scope.items.length;
+        return $scope.list.items.length;
       }
 
       $scope.getRemaining = function(){
         var count = 0;
-        $scope.items.forEach (function(item, index, items){
+        $scope.list.items.forEach (function(item, index, items){
           if(!item.status){
             count = count + 1;
           }
@@ -38,7 +37,7 @@ angular.module('checklist.controllers', [])
 
       $scope.getDone = function(){
         var count = 0;
-        $scope.items.forEach (function(item, index, items){
+        $scope.list.items.forEach (function(item, index, items){
           if(item.status){
             count = count + 1;
           }
@@ -47,21 +46,19 @@ angular.module('checklist.controllers', [])
       }
 
       $scope.getPercent = function(){
-        if ($scope.items.length != 0) {
+        if ($scope.list.items.length != 0) {
           var percent = 100 * ($scope.getDone()/$scope.getTotal());
           return Math.round(percent) + "%";
         }
       }
 
       $scope.getDecimal = function(){
-        if ($scope.items) {
+        if ($scope.list.items.length !=0) {
           var decimal = ($scope.getDone()/$scope.getTotal());
-          return Math.round(decimal);
+          return Math.round(decimal * 10)/10;
         }
       }
 
-    promise.then(function() {
-        console.log(JSON.stringify($scope.items));
-    })
 
+    })
   }]);
