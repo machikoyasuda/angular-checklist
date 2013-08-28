1a3b3e2531d6337ef336f896959cbcfe57ddf286
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-27 17:47:58 -0700

    Show total items; show items remaining.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,20 +3,34 @@
 /* Controllers */
 
 angular.module('checklist.controllers', [])
-  .controller('AddCtrl', [
+  .controller('ItemCtrl', [
     '$scope',
     'angularFire',
     function($scope, angularFire) {
       var url = 'https://mmy-checklist.firebaseio.com/list';
       var promise = angularFire(url, $scope, 'list', {});
 
-      $scope.myVar = "Start your list here.";
       $scope.items = [];
       $scope.addItem = function(){
-        $scope.items.push({text:$scope.text});
+        $scope.items.push({text:$scope.text, status:false});
         $scope.list.items = $scope.items;
         $scope.text = '';
         console.log(JSON.stringify($scope.items));
+        console.log(JSON.stringify($scope.items.length));
+      }
+
+      $scope.getTotal = function(){
+        return $scope.items.length;
+      }
+
+      $scope.getRemaining = function(){
+        var count = 0;
+        $scope.items.forEach (function(item, index, items){
+          if(!item.status){
+            count = count + 1;
+          }
+        });
+        return count
       }
 
     promise.then(function() {
