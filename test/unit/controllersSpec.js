803b0b88c4d96238867edbce5096f0b1bcf2cc00
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-29 12:01:39 -0700

    Start testing configuration. Not done yet.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,14 +3,37 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  beforeEach(module('checklist.controllers'));
 
-
-  it('should ....', inject(function() {
-    //spec body
+  it('should not have any items on start', inject(function() {
+    expect(scope.items.length).toBe(0);
   }));
 
-  it('should ....', inject(function() {
-    //spec body
+  it('should not have any items total on start', inject(function() {
+    expect(scope.getTotal()).toBe(0);
   }));
+
+  it('should not have any items done on start', inject(function() {
+    expect(scope.getDone().toBe(0));
+  }));
+
+  ## it should not allow blank items
+
+  ## it should count total items
+
+  ## it should not get counted after it is checked off
+
+  ## it should show percentage completed, rounded off
+
+  ## it should save items into unique URL
+
+  ## it should have a delete button
+
+  ## the delete button should delete the item
+
+  ## it should have an edit button
+
+  ## the edit button should edit the item name
+
+
 });
