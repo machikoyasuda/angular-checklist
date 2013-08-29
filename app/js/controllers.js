04b1557af9a1d46e68c5e6ec0ea4570603c42593
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-29 16:28:44 -0700

    Add new list create button; create new lists and items within list.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,27 +8,36 @@ angular.module('checklist.controllers', [])
     'angularFire',
     function($scope, angularFire) {
       var url = 'https://mmy-checklist.firebaseio.com/list';
-      var promise = angularFire(url, $scope, 'list', {items: []});
+      var promise = angularFire(url, $scope, 'list', [{items: []}]);
 
       promise.then(function(){
+        $scope.currentList = 0;
+        console.log($scope.currentList);
+
+        $scope.createList = function(){
+          alert("Your group checklist is created!");
+          $scope.list.push({items: []});
+          $scope.currentList = $scope.list.length-1;
+        }
+
         $scope.addItem = function(){
           if($scope.text){
-            $scope.list.items.push({text:$scope.text, status:false});
+            $scope.list[$scope.currentList].items.push({text:$scope.text, status:false});
           }
           $scope.text = '';
         }
 
         $scope.deleteItem = function(){
-          $scope.list.items.splice(this.$index,1);
+          $scope.list[$scope.currentList].items.splice(this.$index,1);
         }
 
         $scope.getTotal = function(){
-          return $scope.list.items.length;
+          return $scope.list[$scope.currentList].items.length;
         }
 
         $scope.getRemaining = function(){
           var count = 0;
-          $scope.list.items.forEach (function(item, index, items){
+          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
             if(!item.status){
               count = count + 1;
             }
@@ -38,7 +47,7 @@ angular.module('checklist.controllers', [])
 
         $scope.getDone = function(){
           var count = 0;
-          $scope.list.items.forEach (function(item, index, items){
+          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
             if(item.status){
               count = count + 1;
             }
@@ -47,14 +56,14 @@ angular.module('checklist.controllers', [])
         }
 
         $scope.getPercent = function(){
-          if ($scope.list.items.length != 0) {
+          if ($scope.list[$scope.currentList].items.length != 0) {
             var percent = 100 * ($scope.getDone()/$scope.getTotal());
             return Math.round(percent) + "%";
           }
         }
 
         $scope.getDecimal = function(){
-          if ($scope.list.items.length !=0) {
+          if ($scope.list[$scope.currentList].items.length !=0) {
             var decimal = ($scope.getDone()/$scope.getTotal());
             return Math.round(decimal * 10)/10;
           }
