dd63775177bf879033606f40617a5ee184a09987
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-29 11:41:01 -0700

    Add delete button to delete items.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,54 +11,53 @@ angular.module('checklist.controllers', [])
       var promise = angularFire(url, $scope, 'list', {items: []});
 
       promise.then(function(){
-      $scope.addItem = function(){
-        if($scope.text){
-          $scope.list.items.push({text:$scope.text, status:false});
-        }
-        $scope.list.items = $scope.list.items;
-        $scope.text = '';
-        // console.log(JSON.stringify($scope.items));
-        // console.log($scope.items.length);
-      }
-
-      $scope.getTotal = function(){
-        return $scope.list.items.length;
-      }
-
-      $scope.getRemaining = function(){
-        var count = 0;
-        $scope.list.items.forEach (function(item, index, items){
-          if(!item.status){
-            count = count + 1;
+        $scope.addItem = function(){
+          if($scope.text){
+            $scope.list.items.push({text:$scope.text, status:false});
           }
-        });
-        return count
-      }
+          $scope.text = '';
+        }
 
-      $scope.getDone = function(){
-        var count = 0;
-        $scope.list.items.forEach (function(item, index, items){
-          if(item.status){
-            count = count + 1;
+        $scope.deleteItem = function(){
+          $scope.list.items.splice(this.$index,1);
+        }
+
+        $scope.getTotal = function(){
+          return $scope.list.items.length;
+        }
+
+        $scope.getRemaining = function(){
+          var count = 0;
+          $scope.list.items.forEach (function(item, index, items){
+            if(!item.status){
+              count = count + 1;
+            }
+          });
+          return count
+        }
+
+        $scope.getDone = function(){
+          var count = 0;
+          $scope.list.items.forEach (function(item, index, items){
+            if(item.status){
+              count = count + 1;
+            }
+          });
+          return count
+        }
+
+        $scope.getPercent = function(){
+          if ($scope.list.items.length != 0) {
+            var percent = 100 * ($scope.getDone()/$scope.getTotal());
+            return Math.round(percent) + "%";
           }
-        });
-        return count
-      }
-
-      $scope.getPercent = function(){
-        if ($scope.list.items.length != 0) {
-          var percent = 100 * ($scope.getDone()/$scope.getTotal());
-          return Math.round(percent) + "%";
         }
-      }
 
-      $scope.getDecimal = function(){
-        if ($scope.list.items.length !=0) {
-          var decimal = ($scope.getDone()/$scope.getTotal());
-          return Math.round(decimal * 10)/10;
+        $scope.getDecimal = function(){
+          if ($scope.list.items.length !=0) {
+            var decimal = ($scope.getDone()/$scope.getTotal());
+            return Math.round(decimal * 10)/10;
+          }
         }
-      }
-
-
-    })
+      })
   }]);
