8ed0e44d1e25deb254c03ec46de18321da84c66c
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-29 18:31:37 -0700

    Separate app into two partials.

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,13 @@
 'use strict';
 
+// Declare app level module which depends on filters, and services
 angular.module('checklist',
   [
     'firebase',
     'checklist.controllers'
-  ]
-);
+  ]).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/create', {templateUrl: 'partials/create.html', controller: 'ItemCtrl'});
+    $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: 'ItemCtrl'});
+    $routeProvider.otherwise({redirectTo: '/create'});
+  }]);
