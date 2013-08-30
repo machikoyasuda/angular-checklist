b0b935e316f2dd699ab7b0ec9c2aa93ce67c9ab3
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-30 13:08:30 -0700

    Start button creates new list and goes to the created list.

--- app/js/app.js
+++ app/js/app.js
@@ -7,8 +7,8 @@ angular.module('checklist',
     'checklist.controllers'
   ]).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/create', {templateUrl: 'partials/create.html', controller: 'ItemCtrl'});
-    $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: 'ItemCtrl'});
+    $routeProvider.when('/', {templateUrl: 'partials/create.html', controller: 'ItemCtrl'});
+    $routeProvider.when('/list/:currentList', {templateUrl: 'partials/list.html', controller: 'ItemCtrl'});
     $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'ItemCtrl'});
-    $routeProvider.otherwise({redirectTo: '/create'});
+    $routeProvider.otherwise({redirectTo: '/'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,68 +6,83 @@ angular.module('checklist.controllers', [])
   .controller('ItemCtrl', [
     '$scope',
     'angularFire',
-    function($scope, angularFire) {
+    '$location',
+    '$routeParams',
+    function($scope, angularFire, $location, $routeParams) {
       var url = 'https://mmy-checklist.firebaseio.com/list';
-      var promise = angularFire(url, $scope, 'list', [{items: ["foo"]}]);
+      var promise = angularFire(url, $scope, 'list', [{items: [{text:"start", status:false}]}]);
 
       promise.then(function(){
-        $scope.currentList = 0;
-        console.log($scope.currentList);
+        $scope.newList = 0;
+        $scope.currentList = $routeParams.currentList;
 
+        // Create new list and add first item
         $scope.createList = function(){
-          alert("Your group checklist is created!");
-          $scope.list.push({items: []});
-          $scope.currentList = $scope.list.length-1;
-
-        }
-
-        $scope.addItem = function(){
-          if($scope.text){
-            $scope.list[$scope.currentList].items.push({text:$scope.text, status:false});
+          if($scope.text != null){
+            alert("Your group checklist is created!");
+            console.log($scope.text);
+            $scope.list.push({items: [{text:$scope.text, status:false}]});
+            $scope.currentList = $scope.list.length-1;
+            console.log($scope.currentList);
+            // $routeParams.currentList = $scope.currentList
+            // console.log($routeParams.currentID);
+            $location.path("/#/list/"+ $scope.currentList);
+            var path  = $location.path();
+            console.log($location.path());
           }
           $scope.text = '';
         }
 
-        $scope.deleteItem = function(){
-          $scope.list[$scope.currentList].items.splice(this.$index,1);
-        }
-
-        $scope.getTotal = function(){
-          return $scope.list[$scope.currentList].items.length;
-        }
-
-        $scope.getRemaining = function(){
-          var count = 0;
-          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
-            if(!item.status){
-              count = count + 1;
-            }
-          });
-          return count
-        }
-
-        $scope.getDone = function(){
-          var count = 0;
-          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
-            if(item.status){
-              count = count + 1;
-            }
-          });
-          return count
-        }
-
-        $scope.getPercent = function(){
-          if ($scope.list[$scope.currentList].items.length != 0) {
-            var percent = 100 * ($scope.getDone()/$scope.getTotal());
-            return Math.round(percent) + "%";
+        // Add more items with button click, view items
+        // Validate: Do not accept empty items
+        $scope.addItem = function(){
+          if($scope.newItem){
+            $scope.list[$scope.currentList].items.push({text:$scope.newItem, status:false});
           }
+          $scope.newItem = '';
         }
 
-        $scope.getDecimal = function(){
-          if ($scope.list[$scope.currentList].items.length !=0) {
-            var decimal = ($scope.getDone()/$scope.getTotal());
-            return Math.round(decimal * 10)/10;
-          }
-        }
+        // Delete a particular item with button click
+        // $scope.deleteItem = function(){
+        //   $scope.list[$scope.currentList].items.splice(this.$index,1);
+        // }
+
+        // $scope.getTotal = function(){
+        //   return $scope.list[$scope.currentList].items.length;
+        // }
+
+        // $scope.getRemaining = function(){
+        //   var count = 0;
+        //   $scope.list[$scope.currentList].items.forEach (function(item, index, items){
+        //     if(!item.status){
+        //       count = count + 1;
+        //     }
+        //   });
+        //   return count
+        // }
+
+        // $scope.getDone = function(){
+        //   var count = 0;
+        //   $scope.list[$scope.currentList].items.forEach (function(item, index, items){
+        //     if(item.status){
+        //       count = count + 1;
+        //     }
+        //   });
+        //   return count
+        // }
+
+        // $scope.getPercent = function(){
+        //   if ($scope.list[$scope.currentList].items.length != 0) {
+        //     var percent = 100 * ($scope.getDone()/$scope.getTotal());
+        //     return Math.round(percent) + "%";
+        //   }
+        // }
+
+        // $scope.getDecimal = function(){
+        //   if ($scope.list[$scope.currentList].items.length !=0) {
+        //     var decimal = ($scope.getDone()/$scope.getTotal());
+        //     return Math.round(decimal * 10)/10;
+        //   }
+        // }
       })
   }]);
