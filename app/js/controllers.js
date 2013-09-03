651f466de8c52f18d2489b96ae796182b2743d70
Machiko Yasuda <machikoyasuda@gmail.com>
2013-09-03 12:04:28 -0700

    Add validation to button.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,15 +17,16 @@ angular.module('checklist.controllers', [])
         $scope.currentList = $routeParams.currentList;
 
         // Create new list and add first item
-        $scope.createList = function(hash){
+        $scope.createList = function(){
           if($scope.text){
-            // TO DO: Fix validation - do not allow empty items
             alert("Your group checklist is created!");
             console.log($scope.text);
             $scope.list.push({items: [{text:$scope.text, status:false}]});
             $scope.currentList = $scope.list.length-1;
             console.log($scope.currentList);
-            $location.path("/list/" + $scope.currentList);
+            window.location.href = "/#/list/" + $scope.currentList
+            // $location.path("/list/" + $scope.currentList);
+            // Disable the url #?
           }
           $scope.text = '';
         }
@@ -51,42 +52,42 @@ angular.module('checklist.controllers', [])
         }
 
         // Calculate list status
-        // $scope.getTotal = function(){
-        //   return $scope.list[$scope.currentList].items.length;
-        // }
+        $scope.getTotal = function(){
+          return $scope.list[$scope.currentList].items.length;
+        }
 
-        // $scope.getRemaining = function(){
-        //   var count = 0;
-        //   $scope.list[$scope.currentList].items.forEach (function(item, index, items){
-        //     if(!item.status){
-        //       count = count + 1;
-        //     }
-        //   });
-        //   return count
-        // }
+        $scope.getRemaining = function(){
+          var count = 0;
+          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
+            if(!item.status){
+              count = count + 1;
+            }
+          });
+          return count
+        }
 
-        // $scope.getDone = function(){
-        //   var count = 0;
-        //   $scope.list[$scope.currentList].items.forEach (function(item, index, items){
-        //     if(item.status){
-        //       count = count + 1;
-        //     }
-        //   });
-        //   return count
-        // }
+        $scope.getDone = function(){
+          var count = 0;
+          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
+            if(item.status){
+              count = count + 1;
+            }
+          });
+          return count
+        }
 
-        // $scope.getPercent = function(){
-        //   if ($scope.list[$scope.currentList].items.length != 0) {
-        //     var percent = 100 * ($scope.getDone()/$scope.getTotal());
-        //     return Math.round(percent) + "%";
-        //   }
-        // }
+        $scope.getPercent = function(){
+          if ($scope.list[$scope.currentList].items.length != 0) {
+            var percent = 100 * ($scope.getDone()/$scope.getTotal());
+            return Math.round(percent) + "%";
+          }
+        }
 
-        // $scope.getDecimal = function(){
-        //   if ($scope.list[$scope.currentList].items.length !=0) {
-        //     var decimal = ($scope.getDone()/$scope.getTotal());
-        //     return Math.round(decimal * 10)/10;
-        //   }
-        // }
+        $scope.getDecimal = function(){
+          if ($scope.list[$scope.currentList].items.length !=0) {
+            var decimal = ($scope.getDone()/$scope.getTotal());
+            return Math.round(decimal * 10)/10;
+          }
+        }
       })
   }]);
