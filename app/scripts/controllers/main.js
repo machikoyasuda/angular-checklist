d6d16309412a5a7efcb959946b4f7351c357f927
Machiko Yasuda <machikoyasuda@gmail.com>
2013-09-04 17:44:02 -0700

    Lint.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,10 @@
 /* Controllers */
 
 angular.module('checklist.controllers', [])
-  .controller('ItemCtrl', [
-    '$scope',
-    'angularFire',
-    '$location',
-    '$routeParams',
+  .controller('ItemCtrl', ['$scope', 'angularFire', '$location', '$routeParams',
     function($scope, angularFire, $location, $routeParams) {
       var url = 'https://mmy-checklist.firebaseio.com/list';
-      var promise = angularFire(url, $scope, 'list', [{items: [{text:"start", status:false}]}]);
+      var promise = angularFire(url, $scope, 'list', [{items: [{text:'start', status:false}]}]);
 
       promise.then(function(){
         $scope.newList = 0;
@@ -18,8 +14,8 @@ angular.module('checklist.controllers', [])
 
         // Go to About page
         $scope.goAbout = function (){
-          $location.path("/about");
-        }
+          $location.path('/about');
+        };
 
         // Add more items with button click, view items
         // Validate: Do not accept empty items
@@ -30,69 +26,65 @@ angular.module('checklist.controllers', [])
             $scope.list[$scope.currentList].items.push({text:$scope.newItem, status:false});
           }
           $scope.newItem = '';
-        }
+        };
 
         // Delete a particular item with button click
         $scope.deleteItem = function(){
           $scope.list[$scope.currentList].items.splice(this.$index,1);
-        }
+        };
 
         // Calculate list status
         $scope.getTotal = function(){
           return $scope.list[$scope.currentList].items.length;
-        }
+        };
 
         $scope.getRemaining = function(){
           var count = 0;
-          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
+          $scope.list[$scope.currentList].items.forEach (function(item){
             if(!item.status){
               count = count + 1;
             }
           });
-          return count
-        }
+          return count;
+        };
 
         $scope.getDone = function(){
           var count = 0;
-          $scope.list[$scope.currentList].items.forEach (function(item, index, items){
+          $scope.list[$scope.currentList].items.forEach (function(item){
             if(item.status){
               count = count + 1;
             }
           });
-          return count
-        }
+          return count;
+        };
 
         $scope.getPercent = function(){
-          if ($scope.list[$scope.currentList].items.length != 0) {
+          if ($scope.list[$scope.currentList].items.length !== 0) {
             var percent = 100 * ($scope.getDone()/$scope.getTotal());
-            return Math.round(percent) + "%";
+            return Math.round(percent) + '%';
           }
-        }
+        };
 
         $scope.getDecimal = function(){
-          if ($scope.list[$scope.currentList].items.length !=0) {
+          if ($scope.list[$scope.currentList].items.length !==0) {
             var decimal = ($scope.getDone()/$scope.getTotal());
             return Math.round(decimal * 10)/10;
           }
-        }
-      })
-  }])
-  .controller('CreateCtrl', [
-    '$scope',
-    'angularFire',
-    '$location',
-    '$routeParams',
-    function($scope, angularFire, $location, $routeParams) {
+        };
+      });
+    }])
+  .controller('CreateCtrl', ['$scope', 'angularFire', '$location', '$routeParams',
+    function($scope, angularFire, $location) {
       // Create new list and add first item
       $scope.createList = function(){
         if($scope.text){
           $scope.list.push({items: [{text:$scope.text, status:false}]});
           $scope.currentList = $scope.list.length-1;
           // console.log($scope.currentList);
-          $location.path("/list/" + $scope.currentList);
-          alert('Your URL is: ' + $location.absUrl());
+          $location.path('/list/' + $scope.currentList);
+          var alert = alert('Your URL is: ' + $location.absUrl());
           return false;
         }
         $scope.text = '';
-      }
+      };
     }]);
